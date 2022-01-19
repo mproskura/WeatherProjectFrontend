6b87ff136f33da46183e1b5996524cf6a94d1fb7
Marek <mproskura@gmail.com>
2022-01-19 10:04:28 +0100

    Maps working

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,10 +3,10 @@ import {MapContainer, TileLayer, Marker, Popup, useMap} from 'react-leaflet';
 import './Map.css';
 
 
-function SetView({coords}) {
+function SetView({coords}, zoom) {
     //console.log(coords.getCenter());
     const map = useMap();
-    map.setView(coords, map.getZoom());
+    map.setView(coords, 12);
     return null;
 }
 
@@ -15,22 +15,24 @@ export default function MyMap(props) {
     let [currentId, setCurrentId] = useState(40);
     let [longitude, setLongitude] = useState(40);
     let [latitude, setLatitude] = useState(0);
+    let [zoom, setZoom] = useState(1);
     if (props.location.id != null && props.location.id !=currentId) {
         setCurrentId(props.location.id);
         setLongitude(props.location.longitude);
         setLatitude(props.location.latitude);
+        setZoom(12);
     }
     console.log("Longitude is set to " + longitude);
     console.log("Latitude is set to " + latitude);
 
     return (
-        <MapContainer center={[longitude, latitude]} zoom={1} scrollWheelZoom={false}>
+        <MapContainer center={[latitude, longitude]} zoom={12} scrollWheelZoom={false}>
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             />
             <SetView
-                coords={[longitude, latitude]}
+                coords={[latitude, longitude]}
             />
         </MapContainer>
     );
